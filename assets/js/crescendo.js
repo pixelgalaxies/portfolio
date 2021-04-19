b787dd1c4aa98eb8916bd024d57afd0595136357
pixelgalaxies <pixel.galaxies@gmail.com>
2021-04-18 17:59:27 -0700

    crescendo again

--- assets/js/crescendo.js
+++ assets/js/crescendo.js
@@ -1,37 +1,48 @@
-// function for navigation
-if (document.querySelector(".nav-icon")) {
-	document.querySelector(".nav-icon").addEventListener("click", function() {
-		document.querySelector(".nav-list").classList.toggle("mobile-show");
-		document.querySelector(".nav-icon").classList.toggle("close");
-		if (document.querySelector(".nav-icon").classList.contains("close")) {
-			document.querySelector(".nav-icon--close").style.display = "inherit";
-			document.querySelector(".nav-icon--menu").style.display = "none";
-		} else {
-			document.querySelector(".nav-icon--close").style.display = "none";
-			document.querySelector(".nav-icon--menu").style.display = "inherit";
-		}
-	});
+// Javascript for Crescendo
+// Author: Dustin Clark
+
+
+//displays dropdown items and switches icon
+function menuOpen() {
+	document.getElementsByClassName("dropdown")[0].style.display="block";
+	
+	document.getElementsByClassName("menu-icon")[0].style.display="none";
+	
+	document.getElementsByClassName("close-icon")[0].style.display="inherit";
 }
 
-// function for topples
-const folds = Array.from(document.querySelectorAll(".fold"));
-if (folds.length > 0) {
-	for (let i = 0; i < folds.length; i++) {
-		folds[i].addEventListener("click", function() {
-			let dataFold = this.getAttribute("data-fold");
-			let toFold = document.querySelector("#" + dataFold);
-			let fold = folds[i];
-			if (toFold.classList.contains("folded")) {
-				toFold.classList.remove("folded");
-				toFold.classList.add("unfolded");
-				fold.classList.remove("folded");
-				fold.classList.add("unfolded");
-			} else {
-				toFold.classList.remove("unfolded");
-				toFold.classList.add("folded");
-				fold.classList.remove("unfolded");
-				fold.classList.add("folded");
-			}
-		});
-	}
+
+//closes dropdown items and switches icons
+function menuClose() {
+	document.getElementsByClassName("dropdown")[0].style.display="none";
+	
+	document.getElementsByClassName("menu-icon")[0].style.display="inherit";
+	
+	document.getElementsByClassName("close-icon")[0].style.display="none";
 }
+
+//this function is called every time the window is resized
+//will hide everything that needs to be hidden based on window size
+function hide() {
+	if (window.innerWidth < 768) {
+		document.getElementsByClassName("menu-icon")[0].style.display="inherit";
+		
+		document.getElementsByClassName("close-icon")[0].style.display="none";
+		
+		document.getElementsByClassName("dropdown")[0].style.display="none";
+		
+		document.getElementsByClassName("nav-links")[0].style.display="none";
+	}
+	else {
+		document.getElementsByClassName("menu-icon")[0].style.display="none";
+		
+		document.getElementsByClassName("close-icon")[0].style.display="none";
+		
+		document.getElementsByClassName("dropdown")[0].style.display="none";
+		
+		document.getElementsByClassName("nav-links")[0].style.display="inherit";
+	}
+	
+}
+
+window.addEventListener("resize", hide);

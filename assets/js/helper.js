ba0d62cf088769150505b50e155a4538d48309d5
pixelgalaxies <pixel.galaxies@gmail.com>
2022-10-03 15:39:53 -0700

    fall 2022 update

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,26 +1,67 @@
-const smoothLinks = document.querySelectorAll(".nav-list a");
-for (const link of smoothLinks) {
-	link.addEventListener("click", clickHandler);
-}
-function clickHandler(e) {
-	e.preventDefault();
-	const href = this.getAttribute("href");
-	const offsetTop = document.querySelector(href).offsetTop;
+// document.querySelector('.toTop').addEventListener("click", function () {
+// 	window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+// })
 
-	scroll({
-		top: offsetTop,
-		behavior: "smooth"
-	});
-}
-
-document.querySelector('.toTop').addEventListener("click", function () {
-
-	window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+// show/hide settings
+document.querySelector(".js-settings").addEventListener("click", function() {
+	document.querySelector(".settings__list").classList.toggle("settings__list--hide");
 })
 
-document.querySelector('.toggleMode').addEventListener("click", function () {
+// mode
+document.querySelector(".js-mode").addEventListener("click", function() {
+	if(document.querySelector("body").classList.contains("dark-mode")) {
+		document.querySelector(".js-mode .btn-text").innerHTML = "Dark Mode";
+	} else {
+		document.querySelector(".js-mode .btn-text").innerHTML = "Light Mode";
+	}
+	document.querySelector(".js-mode .btn-icon i").classList.toggle("fa-sun");
+	document.querySelector(".js-mode .btn-icon i").classList.toggle("fa-moon");
+	document.querySelector("body").classList.toggle("dark-mode");
+})
 
-	document.querySelector('body').classList.toggle("dark")
-	document.querySelector('.toggleMode i').classList.toggle("fa-sun")
-	document.querySelector('.toggleMode i').classList.toggle("fa-moon")
+// motion
+document.querySelector(".js-motion").addEventListener("click", function() {
+	if(document.querySelector("body").classList.contains("disable-motion")) {
+		document.querySelector(".js-motion .btn-text").innerHTML = "Disable Motion";
+	} else {
+		document.querySelector(".js-motion .btn-text").innerHTML = "Enable Motion";
+	}
+	document.querySelector("body").classList.toggle("disable-motion");
+	document.querySelector(".js-motion .btn-icon i").classList.toggle("fa-stop");
+	document.querySelector(".js-motion .btn-icon i").classList.toggle("fa-play");
+})
+
+// hero circles
+var html = '';
+for (var i = 1; i <= 25; i ++) {
+  if (i % 3 == 0) {
+    html += '<div class="circle circle--'+i+' circle-reverse"></div>';
+  } else {
+    html += '<div class="circle circle--'+i+'"></div>';
+  }
+  
+}
+document.querySelector('.circles').innerHTML += html;
+
+// fixed header
+window.addEventListener("scroll", function () {
+	if(window.scrollY > 100) {
+		document.querySelector("header").classList.add("fixed");
+	} else {
+		document.querySelector("header").classList.remove("fixed");
+	}
+})
+
+// remove cutout on header
+window.addEventListener('resize', function() {
+	if(window.innerWidth > 1024) {
+		document.querySelector("header").classList.add("p-cutout-5");
+	} else {
+		document.querySelector("header").classList.remove("p-cutout-5");
+	}
+})
+
+// nav
+document.querySelector(".nav__icon").addEventListener("click", function () {
+	document.querySelector(".nav").classList.toggle("nav-open");
 })
